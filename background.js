2ef6a92c39aba178cd18417562878dafcecabe85
Wendy Wu <YicunWendyWu@users.noreply.github.com>
2021-11-27 10:16:21 +0800

    save current tab to pocket

---
+++ background.js
@@ -0,0 +1,25 @@
+function wait(timeout) {
+    return new Promise(resolve => {
+        setTimeout(resolve, timeout);
+    });
+}
+
+async function closeTab(tab) {
+  await wait(5000);
+  chrome.tabs.remove(tab.id);
+}
+
+function sendToPocket(tabs) {
+  tabs.forEach(function(chrometab) {
+    var url = encodeURIComponent(chrometab.url);
+    var id = chrometab.id;
+    var prefix = "http://getpocket.com/edit?url=";
+    var newurl = prefix + url;
+    chrome.tabs.update(id, { url: newurl }, closeTab);
+  });
+}
+
+// Called when the user clicks on the browser action icon.
+chrome.browserAction.onClicked.addListener(function(tab) {
+  sendToPocket([tab]);
+});
